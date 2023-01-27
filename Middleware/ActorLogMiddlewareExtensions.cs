064f0f6bcb20b446aa12d5f0e7869682d340fdf5
d-a-tacito <dananto02@gmail.com>
2023-01-27 15:35:10 +0300

    updated MoviesApp

---
+++ Middleware/ActorLogMiddlewareExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Builder;
+namespace MoviesApp.Middleware;
+public static class ActorLogMiddlewareExtensions
+{
+    public static IApplicationBuilder UseActorLog(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ActorLogMiddleware>();
+    }
+}
